0408ac4ae342009964d8fd7c5f64d337e6b42d68
ot-babs <toyosi.babs@gmail.com>
2023-09-18 08:30:50 +0100

    Added Dynamic Temperature. Added ability to change city, try and except and images for weather conditions.

---
+++ backend.py
@@ -0,0 +1,17 @@
+import requests
+
+API_KEY = "1ff53725a750f08f3a22a98d3181ad95"
+
+
+def get_data(place, forecast_days=None):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
